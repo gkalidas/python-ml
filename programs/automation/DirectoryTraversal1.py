022e2a8d5b9d4baba720e14aa1d993c1aa7bf881
gkalidas <ganeshlondhe2020@gmail.com>
2025-06-08 19:46:42 +0530

    Directroy traversal using os.walk

---
+++ programs/automation/DirectoryTraversal1.py
@@ -0,0 +1,13 @@
+import os
+
+def main():
+    for folder_name, sub_folder_names, file_names in os.walk("marvellous"):
+        print("Folder name is ", folder_name)
+        for sub_folder in sub_folder_names:
+            print("Sub-folder name is ", sub_folder)
+        for file_name in file_names:
+            print("File name is ", file_name)
+
+
+if __name__ == "__main__":
+    main()
