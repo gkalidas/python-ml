2d23ead94879fb24aa824d30df566b005a78a3ce
gkalidas <ganeshlondhe2020@gmail.com>
2025-06-08 20:36:19 +0530

    Directory traversal, read file names

---
+++ programs/automation/DirectoryTraversal5.py
@@ -0,0 +1,35 @@
+import os
+
+def directory_watcher(dir_name = "marvellous"):
+    flag = os.path.isabs(dir_name)
+
+    if not flag:
+        dir_name = os.path.abspath(dir_name)
+    
+    flag = os.path.exists(dir_name)
+    if not flag:
+        print("The path is invalid.")
+        exit()
+
+    flag = os.path.isdir(dir_name)
+    if not flag:
+        print("The is valid, but the target is not directory.")
+        exit()
+
+    print("Absolute path is ", dir_name)
+    for folder_name, sub_folder_names, file_names in os.walk(dir_name):
+        for file_name in file_names:
+            print("File name is ", file_name)
+            file_name = dir_name + "\\" + folder_name + "\\" + file_name
+            print(os.path.join(folder_name, file_name))
+            # print("File size is ", os.path.getsize(file_name))
+
+
+def main():
+    print("Enter the name of the directory that you want to traverse : ")
+    dir_name = input()
+    directory_watcher(dir_name)
+
+
+if __name__ == "__main__":
+    main()
